35abcbc29de8b61a2ed45f51a4e85bd49511cd29
Alex-Adam25 <68447567+Alex-Adam25@users.noreply.github.com>
2020-07-18 23:35:12 +0200

    Update EP2_Transmittanz.m

--- EP2_Transmittanz.m
+++ EP2_Transmittanz.m
@@ -33,17 +33,17 @@ function EP2_Transmittanz
 
      % Update the plot YData with these new values. XData stays the same
      hyeinP.YData = yein;
-	 hyeinP.Color = [abs(sin(i*0.2+pi/3)) abs(cos(i*0.1)) abs(sin(i*0.4))];
-	 hyeinP.LineWidth = abs(sin(i*0.1))*10;
+	 %hyeinP.Color = [abs(sin(i*0.2+pi/3)) abs(cos(i*0.1)) abs(sin(i*0.4))];
+	 %hyeinP.LineWidth = abs(sin(i*0.1))*10;
 	 
 	 hyreflP.YData = yrefl;
-	 hyreflP.Color = [abs(sin(i*0.2+pi/3)) abs(cos(i*0.3)) abs(sin(i*0.4))];
-	 hyreflP.LineWidth = abs(sin(i*0.1+pi/2))*30;
+	 %hyreflP.Color = [abs(sin(i*0.2+pi/3)) abs(cos(i*0.3)) abs(sin(i*0.4))];
+	 %hyreflP.LineWidth = abs(sin(i*0.1+pi/2))*30;
 	 
 	 hysuperP.YData = ysuper;
-	 hysuperP.Color = [abs(sin(i*0.2+pi/2)) abs(cos(i*0.2)) abs(sin(i*0.4))];
+	 %hysuperP.Color = [abs(sin(i*0.2+pi/2)) abs(cos(i*0.2)) abs(sin(i*0.4))];
 	 hytransP.YData = ytrans;
-	 hytransP.Color = [abs(sin(i*0.2+pi/6)) abs(cos(i*0.5)) abs(sin(i*0.4))];
+	 %hytransP.Color = [abs(sin(i*0.2+pi/6)) abs(cos(i*0.5)) abs(sin(i*0.4))];
      % Render the updated values to the screen
      drawnow;
      % Adjust the pause time to change the framerate (within reason).
